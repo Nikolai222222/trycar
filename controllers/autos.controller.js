388f1cdfe0141193cd000447781458672e9333f9
nikolai222222 <niko.aguirre@duocuc.cl>
2024-05-28 13:09:23 -0400

    agregue mas info

---
+++ controllers/autos.controller.js
@@ -0,0 +1,46 @@
+const { request, response } =require("express")
+const{autos} =require("../autos");
+
+
+const getAuto=(req=request, res=response)=>{
+    return res.json({
+        status:true,
+        robots,
+        statusCode : 200
+    });
+}
+
+const getAutoById=(req=request, res=response)=>{
+    let id = req.params.id;
+
+    if(!req.params.id){
+        return res.json({
+            msg:"debe agregar un id del área para realizar la busqueda"
+        });
+    }
+
+    let autoABuscar ="";
+
+    autoABuscar = autos.find((autos)=>{
+        return autos.id === id
+    });
+
+    if(autoABuscar){
+        return res.json({
+            ok:true,
+            autoABuscar,
+            statusCode:200
+        })
+    }else{
+        return res.json({
+            ok:false,
+            msg:"no hay autos con esta id",
+            statusCode:404
+        });
+    }
+}
+
+module.exports ={
+    getAuto,
+    getAutoById
+}
