dcd0cff8a66195e957d0ceb947a7e7e3218a7a03
nikolai222222 <niko.aguirre@duocuc.cl>
2024-05-28 13:05:15 -0400

    trycar

---
+++ autos.js
@@ -0,0 +1,16 @@
+const autos = [
+
+    {
+     "series":1,
+     "id": "001",
+     "name": "bmw",
+     "avatar":"https://i.pinimg.com/originals/55/e5/45/55e5459f90675afb36a06ff650040655.jpg",
+     "model": "E46",
+     "year": "2004",
+     "description":".."
+    }
+ ];
+ 
+ module.exports={
+     autos
+ }
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app=express();
+const port = 3001;
+
+const { getAuto, getAutoById } =require("./controllers/autos.controller");
+
+app.get("/", getAuto);
+
+app.get("/:id", getAutoById);
+
+app.listen(port,()=>{
+    console.log("ahora si");
+    console.log(`conectado exitosamente: ${port}`);
+})
